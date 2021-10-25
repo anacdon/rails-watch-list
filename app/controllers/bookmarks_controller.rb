d6d9fc680f312f8a07648f072749cfa079ef1ee3
Ana Nascimento <anacdon@yahoo.com.br>
2021-10-25 14:06:38 -0300

    quase finalizando bookmark

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,7 +1,30 @@
 class BookmarksController < ApplicationController
+  before_action :find_list
+
   def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@bookmark.list)
+    else
+      render :new
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:movie, :comment)
+  end
+
+  def find_list
+    @list = List.find(params[:list_id])
   end
 end
